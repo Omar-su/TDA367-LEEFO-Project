829dae9f000bba5b894cd18ef33aa6756a543d58
Emelie Edberg <familjen.emelie@gmail.com>
2021-09-15 17:40:55 +0200

    Created bottom_navigation.xml. Added a bottom_navigation and button to the activity_main.xml.

--- app/src/main/java/com/leefo/budgetapplication/view/MainActivity.java
+++ app/src/main/java/com/leefo/budgetapplication/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.leefo.budgetapplication;
+package com.leefo.budgetapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.leefo.budgetapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
