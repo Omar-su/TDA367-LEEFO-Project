3d893db66e53b60bbacecb2a1215be342e57649c
Emelie Edberg <familjen.emelie@gmail.com>
2021-09-15 21:15:21 +0200

    changed an id an a few colors and added new color in color.xml

--- app/src/main/java/com/leefo/budgetapplication/view/MainActivity.java
+++ app/src/main/java/com/leefo/budgetapplication/view/MainActivity.java
@@ -1,8 +1,11 @@
 package com.leefo.budgetapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.leefo.budgetapplication.R;
 
@@ -12,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // color example
+        TextView textView;
+        //textView.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500));
+        //textView.setBackgroundColor(Color.parseColor("#A0A0A0"));
     }
+
+
 }
