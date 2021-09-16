3d3e10d2ab77013441e2a871ca85c5dbb2095bf4
Emelie Edberg <familjen.emelie@gmail.com>
2021-09-16 09:43:59 +0200

    added on item clicked method for the bottomnavigation. added frameLayouts. Added BudgetFragment. BudgetFragment cannot be opened yet. Added makeToas method to help test new code.

--- app/src/main/java/com/leefo/budgetapplication/view/MainActivity.java
+++ app/src/main/java/com/leefo/budgetapplication/view/MainActivity.java
@@ -2,11 +2,14 @@ package com.leefo.budgetapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.leefo.budgetapplication.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +23,41 @@ public class MainActivity extends AppCompatActivity {
         TextView textView;
         //textView.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500));
         //textView.setBackgroundColor(Color.parseColor("#A0A0A0"));
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_budget:
+                    makeToast("Budget was clicked");
+                    fragment = new BudgetFragment();
+                    break;
+
+                case R.id.nav_home:
+                    makeToast("Home was clicked");
+                    //fragment = new BudgetFragment();
+                    break;
+
+                case R.id.nav_more:
+                    makeToast("More was clicked");
+                    //fragment = new BudgetFragment();
+                    break;
+            }
+
+            //getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout_large, fragment).commit();
+
+            return true;
+        });
+
+    }
+
+    //Use to test
+    Toast t;
+    private void makeToast(String s){
+        if(t != null) t.cancel();
+        t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        t.show();
     }
 
 
