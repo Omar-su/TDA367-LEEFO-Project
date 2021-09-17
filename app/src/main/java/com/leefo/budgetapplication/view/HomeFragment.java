24f0e44f3997c7f931a05b4daedb5dc1783914ba
Emelie Edberg <familjen.emelie@gmail.com>
2021-09-17 22:01:48 +0200

    added a togglebutton to switch beteween list and category fragment. The design needs work though.

--- app/src/main/java/com/leefo/budgetapplication/view/HomeFragment.java
+++ app/src/main/java/com/leefo/budgetapplication/view/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.ToggleButton;
 
 import androidx.fragment.app.Fragment;
 
@@ -21,6 +23,17 @@ public class HomeFragment extends Fragment {
         // Start with displaying category fragment
         getActivity().getSupportFragmentManager().beginTransaction().add(R.id.FrameLayout_middleSection_Home, new HomeCategoryViewFragment()).commit();
 
+        ToggleButton toggle = view.findViewById(R.id.toggleButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    getActivity().getSupportFragmentManager().beginTransaction().add(R.id.FrameLayout_middleSection_Home, new HomeListViewFragment()).commit();
+
+                } else {
+                    getActivity().getSupportFragmentManager().beginTransaction().add(R.id.FrameLayout_middleSection_Home, new HomeCategoryViewFragment()).commit();
+                }
+            }
+        });
 
         return view;
     }
