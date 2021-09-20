39e7c4f39ab97b03bc476c37ae7d8c4fed6d4ba2
Emelie Edberg <familjen.emelie@gmail.com>
2021-09-20 14:13:46 +0200

    pieChart

--- app/src/main/java/com/leefo/budgetapplication/view/HomeCategoryViewFragment.java
+++ app/src/main/java/com/leefo/budgetapplication/view/HomeCategoryViewFragment.java
@@ -1,5 +1,6 @@
 package com.leefo.budgetapplication.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,13 +8,74 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.leefo.budgetapplication.R;
+
+import java.util.ArrayList;
 
 
 public class HomeCategoryViewFragment extends Fragment {
 
+    PieChart pieChart;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_category_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_category_view, container, false);
+
+        pieChart = view.findViewById(R.id.pie_chart);
+        setupPieChart();
+        loadPieChartData();
+        return view;
+    }
+
+    private void setupPieChart(){
+        pieChart.setDrawHoleEnabled(true); // donut
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText("Spending by category");
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+
+    private void loadPieChartData(){
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.4f,"Mat"));
+        entries.add(new PieEntry(0.3f,"Transpport"));
+        entries.add(new PieEntry(0.3f,"Utbildning"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS){
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS){
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate(); // update
     }
 }
