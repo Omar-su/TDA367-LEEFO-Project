79b6188bc1a8996ab7a40b83e0f4405eb1815a9c
felixedholm <felix.edholm@gmail.com>
2021-09-20 15:23:40 +0200

    Added tests for adding and removing transactions. Added tests for changing amount of a transaction.

--- app/src/test/java/com/leefo/budgetapplication/DataHandlerTest.java
+++ app/src/test/java/com/leefo/budgetapplication/DataHandlerTest.java
@@ -4,5 +4,54 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import androidx.constraintlayout.helper.widget.MotionEffect;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+//Break out some tests into separate classes when model classes are clear
 public class DataHandlerTest {
+
+    @Test
+    public void addedTransactionExistsInDatabase(){
+        Transaction testTransaction = new Transaction(1, 14.5, "Food from mcDonalds", 20210203, 3);
+        DataHandler dh = new DataHandler();
+        dh.addTransaction(testTransaction);
+        List<Transaction> transactionList = dh.getTransactions();
+        assertTrue(transactionList.contains(testTransaction));
+    }
+
+    @Test
+    public void removedTransactionDoesntExistDatabase(){
+        Transaction testTransaction = new Transaction(1, 14.5, "Food from mcDonalds", 20210203, 3);
+        DataHandler dh = new DataHandler();
+        dh.addTransaction(testTransaction);
+        List<Transaction> transactionList = dh.getTransactions();
+        if(transactionList.contains(testTransaction)){
+            dh.removeTransaction(testTransaction);
+        }else{
+            fail("testTransaction wasn't added correctly.");
+        }
+        assertFalse(transactionList.contains(testTransaction));
+    }
+
+    @Test
+    public void cantSetAmountAsText(){
+        double amount = 14.5;
+        Transaction testTransaction = new Transaction(1, amount, "Food from mcDonalds", 20210203, 3);
+        TransactionHandler th = new TransactionHandler();
+        th.editAmount("ssta");
+        assertEquals(amount, testTransaction.getAmount());
+    }
+
+    @Test
+    public void canChangeAmount(){
+        double oldAmount = 14.5;
+        Transaction testTransaction = new Transaction(1, oldAmount, "Food from mcDonalds", 20210203, 3);
+        TransactionHandler th = new TransactionHandler();
+        double newAmount = 17.0;
+        th.editAmount(newAmount);
+        assertEquals(newAmount, testTransaction.getAmount());
+
+    }
 }
