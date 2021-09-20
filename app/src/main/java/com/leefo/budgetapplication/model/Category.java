44e5462844c01b0f9ca2b541ce7ce57011d88a70
dubbell <46517201+dubbell@users.noreply.github.com>
2021-09-20 10:44:42 +0200

    basic functionality for transaction and category container classes

--- app/src/main/java/com/leefo/budgetapplication/model/Category.java
+++ app/src/main/java/com/leefo/budgetapplication/model/Category.java
@@ -1,4 +1,66 @@
 package com.leefo.budgetapplication.model;
 
+
+import androidx.annotation.NonNull;
+
+/*
+* Immutable container class for category data
+* */
 public class Category {
+
+    private final int id; // unique id assigned by db
+    private final String name;
+    private final String color;
+
+
+    public Category(int id, String name, String color)
+    {
+        this.id = id;
+        this.name = name;
+        this. color = color;
+    }
+
+
+    // ----------
+
+    // returns true if given transaction is of this category
+    public boolean transactionBelongs(Transaction transaction)
+    {
+        return id == transaction.getCategoryId();
+    }
+
+    // compares two categories
+    public boolean Equals(Category cat)
+    {
+        return cat.getId() == id;
+    }
+
+    @NonNull
+    @Override
+    public String toString()
+    {
+        return "" + id + " : " + name + ", color : " + color;
+    }
+
+
+
+
+    // GETTERS -------
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+
 }
