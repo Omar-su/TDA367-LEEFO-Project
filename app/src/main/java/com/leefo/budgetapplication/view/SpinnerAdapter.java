1c9a2b7a7682be4dc059d5e98c19b1c13a7f64f4
Emelie Edberg <familjen.emelie@gmail.com>
2021-09-21 16:10:18 +0200

    spinner for choosing category is finished

--- app/src/main/java/com/leefo/budgetapplication/view/SpinnerAdapter.java
+++ app/src/main/java/com/leefo/budgetapplication/view/SpinnerAdapter.java
@@ -1,14 +1,63 @@
 package com.leefo.budgetapplication.view;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.leefo.budgetapplication.R;
 import com.leefo.budgetapplication.model.CategoryFake;
 
+import java.util.ArrayList;
+
+
 public class SpinnerAdapter extends ArrayAdapter<CategoryFake> {
-    public SpinnerAdapter(@NonNull Context context, int resource, @NonNull CategoryFake[] objects) {
-        super(context, resource, objects);
+
+    LayoutInflater layoutInflater;
+
+    public SpinnerAdapter(@NonNull Context context, @NonNull ArrayList<CategoryFake> categories) {
+        super(context, R.layout.spinner_row, categories);
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View rowView = layoutInflater.inflate(R.layout.spinner_row, null, true);
+        CategoryFake categoryFake = getItem(position);
+
+        TextView name = rowView.findViewById(R.id.category_name);
+        View circle = rowView.findViewById(R.id.circle);
+
+        name.setText(categoryFake.getName());
+        circle.getBackground().setColorFilter(Color.parseColor(categoryFake.getColor()), PorterDuff.Mode.SRC_ATOP);
+
+
+        return rowView;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null){
+            convertView = layoutInflater.inflate(R.layout.spinner_row, parent, false);
+
+        }
+        CategoryFake categoryFake = getItem(position);
+
+        TextView name = convertView.findViewById(R.id.category_name);
+        View circle = convertView.findViewById(R.id.circle);
+
+        name.setText(categoryFake.getName());
+        circle.getBackground().setColorFilter(Color.parseColor(categoryFake.getColor()), PorterDuff.Mode.SRC_ATOP);
+
+
+        return convertView;
     }
 }
