ee07e9ae1960d8cdb1153be911d73a957416a441
Staol <kolontayev1@gmail.com>
2021-09-25 08:14:32 +0200

    Created Bar Graph

--- app/src/main/java/com/leefo/budgetapplication/view/BudgetFragment.java
+++ app/src/main/java/com/leefo/budgetapplication/view/BudgetFragment.java
@@ -8,12 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.leefo.budgetapplication.R;
+
+import java.util.ArrayList;
 
 /**
  * Class that represents the fragment for the Budget page
  */
 public class BudgetFragment extends Fragment {
+
+    BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
 
     /**
      * Method that runs when the fragment is being created.
