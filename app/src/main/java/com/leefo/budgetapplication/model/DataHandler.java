bf3a4c35fb6a52c75b1f61824bfcd3c82f0a9f52
felixedholm <felix.edholm@gmail.com>
2021-09-28 10:14:37 +0200

    Added methods to DataHandler class

--- app/src/main/java/com/leefo/budgetapplication/model/DataHandler.java
+++ app/src/main/java/com/leefo/budgetapplication/model/DataHandler.java
@@ -1,4 +1,31 @@
 package com.leefo.budgetapplication.model;
 
+import java.util.List;
+
 public class DataHandler {
+
+    private final List<Transaction> transactionList;
+
+    private final List<Category> categoryList;
+
+
+    public DataHandler(List<Transaction> transactionList, List<Category> categoryList) {
+        this.transactionList = transactionList;
+        this.categoryList = categoryList;
+    }
+
+    public void addTransaction(Transaction transaction){
+        transactionList.add(transaction);
+    }
+
+    public void deleteTransaction(Transaction transaction){
+        transactionList.remove(transaction);
+    }
+
+    public void addCategory(Category category){
+        categoryList.add(category);
+    }
+
+    public void deleteCategory(Category category){
+    }
 }
