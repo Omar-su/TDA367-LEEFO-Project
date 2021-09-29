fd06b544bea8e81dcd073ba278dc9dba61cd55ba
Staol <kolontayev1@gmail.com>
2021-09-29 16:11:34 +0200

    Generated initRadioGroup method in ManageCategoriesFragment

--- app/src/main/java/com/leefo/budgetapplication/view/ManageCategoriesFragment.java
+++ app/src/main/java/com/leefo/budgetapplication/view/ManageCategoriesFragment.java
@@ -12,7 +12,12 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 
 import com.leefo.budgetapplication.R;
+import com.leefo.budgetapplication.model.Category;
 import com.leefo.budgetapplication.view.adapters.ListViewAdapterHomeList;
+import com.leefo.budgetapplication.view.adapters.ManageCategoriesListAdapter;
+import com.leefo.budgetapplication.view.adapters.SpinnerAdapter;
+
+import java.util.ArrayList;
 
 
 public class ManageCategoriesFragment extends Fragment {
@@ -20,14 +25,33 @@ public class ManageCategoriesFragment extends Fragment {
     private Button newCategoryButton;
     private ListView listView;
     private RadioGroup radioGroup;
+    private ManageCategoriesListAdapter adapter;
+
+    private ArrayList<Category> getIncomeCategories;
+    private ArrayList<Category> getOutcomeCategories;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_manage_categories, container, false);
 
-        //adapter = new ListViewAdapterHomeList(getActivity().getApplicationContext(),transactions);
+        initRadioGroup();
+        adapter = new ManageCategoriesListAdapter(getActivity().getApplicationContext(), );
         //listView.setAdapter(adapter);
         return view;
+    }
+
+    public void initRadioGroup(){
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioExpense){
+                    adapter = new ManageCategoriesListAdapter(getActivity().getApplicationContext(), getIncomeCategories);
+                } else {
+                    adapter = new ManageCategoriesListAdapter(getActivity().getApplicationContext(), getOutcomeCategories);
+                }
+            }
+        });
     }
 
 
