073005a532e533517faf0032e94b94f5527b2694
Staol <kolontayev1@gmail.com>
2021-09-30 16:43:02 +0200

    Removed unnecessary buttons from MoreFragment. Changed the buttons to open their fragments directly from MainActivity.java

--- app/src/main/java/com/leefo/budgetapplication/view/fragments/MoreFragment.java
+++ app/src/main/java/com/leefo/budgetapplication/view/fragments/MoreFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.leefo.budgetapplication.R;
+import com.leefo.budgetapplication.view.MainActivity;
 
 /**
  * Class that represents the fragment for the More page
@@ -17,8 +18,6 @@ import com.leefo.budgetapplication.R;
 public class MoreFragment extends Fragment {
 
     private Button btnCompareMonths;
-    private Button btnEditCategory;
-    private Button btnNewCategory;
     private Button btnManageCategory;
     /**
      * Method that runs when the fragment is being created.
@@ -29,12 +28,8 @@ public class MoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_more, container, false);
         btnCompareMonths = view.findViewById(R.id.compare_month_button);
-        btnEditCategory = view.findViewById(R.id.edit_category_button);
-        btnNewCategory = view.findViewById(R.id.new_category_button);
         btnManageCategory = view.findViewById(R.id.manage_categories_button);
         init_btnCompareMonths_OnClickListener();
-        init_btnNewCategory_OnClickListener();
-        init_btnEditCategory_OnClickListener();
         init_btnManageCategory_OnClickListener();
         return view;
     }
@@ -43,65 +38,16 @@ public class MoreFragment extends Fragment {
         btnManageCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hideAllButtons();
-                FragmentManager fragmentManager = getChildFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.FrameLayout_more, ManageCategoriesFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack(null)
-                        .commit();
+                ((MainActivity)getActivity()).openFragmentInMainFrameLayout(new ManageCategoriesFragment());
             }
         });
     }
-    private void init_btnEditCategory_OnClickListener(){
-        btnEditCategory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideAllButtons();
-                FragmentManager fragmentManager = getChildFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.FrameLayout_more, EditCategoryFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack(null)
-                        .commit();
-            }
 
-
-        });
-    }
-
-    private void hideAllButtons() {
-        btnCompareMonths.setVisibility(View.GONE);
-        btnEditCategory.setVisibility(View.GONE);
-        btnNewCategory.setVisibility(View.GONE);
-        btnManageCategory.setVisibility(View.GONE);
-    }
-
-    private void init_btnNewCategory_OnClickListener() {
-        btnNewCategory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideAllButtons();
-                FragmentManager fragmentManager = getChildFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.FrameLayout_more, NewCategoryFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-    }
     private void init_btnCompareMonths_OnClickListener() {
         btnCompareMonths.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hideAllButtons();
-                FragmentManager fragmentManager = getChildFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.FrameLayout_more, CompareMonthsFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack(null)
-                        .commit();
+                ((MainActivity)getActivity()).openFragmentInMainFrameLayout(new CompareMonthsFragment());
             }
         });
     }
