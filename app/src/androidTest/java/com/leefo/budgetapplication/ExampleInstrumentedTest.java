743d81a69650d67fb896c65b31910e48b796aca6
felixedholm <felix.edholm@gmail.com>
2021-10-04 15:53:45 +0200

    Added TransactionModelTest class to instrumentation test package to test with database.

--- app/src/androidTest/java/com/leefo/budgetapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/leefo/budgetapplication/ExampleInstrumentedTest.java
@@ -11,11 +11,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.leefo.budgetapplication.controller.Controller;
 import com.leefo.budgetapplication.model.Category;
 import com.leefo.budgetapplication.model.DataBaseManager;
+import com.leefo.budgetapplication.model.FinancialTransaction;
+import com.leefo.budgetapplication.model.TransactionModel;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -25,17 +29,43 @@ import java.util.List;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Before
-    public void init() {
-        Context c = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        DataBaseManager dataBaseManager = new DataBaseManager(c);
-    }
+
+    DataBaseManager db;
+
+    TransactionModel tm;
+
+    FinancialTransaction testTransaction1;
+    Category testCategory1;
+    LocalDate testDate1;
 
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.leefo.budgetapplication", appContext.getPackageName());
+    }
+
+    @Before
+    public void init(){
+        Context c = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        db = new DataBaseManager(c);
+        tm = new TransactionModel(db);
+        testCategory1 = new Category("Test", "#FFFFFF", true);
+        testDate1 = LocalDate.now();
+        testTransaction1 = new FinancialTransaction((float)16.9, "test", testDate1,
+                testCategory1);
+    }
+
+    @Test
+    public void canAddTransaction() {
+        tm.addTransaction(testTransaction1);
+
+        assertTrue(tm.getTransactionList().contains(testTransaction1));
+    }
+
+    @Test
+    public void canRemoveTransaction() {
+
     }
 
 
