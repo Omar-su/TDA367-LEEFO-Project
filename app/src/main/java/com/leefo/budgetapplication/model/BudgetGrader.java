f56cd2539936838d40732daec1ada2ab66b3e08a
felixedholm <felix.edholm@gmail.com>
2021-10-12 10:38:51 +0200

    Added methods for grading to BudgetGrader class.

--- app/src/main/java/com/leefo/budgetapplication/model/BudgetGrader.java
+++ app/src/main/java/com/leefo/budgetapplication/model/BudgetGrader.java
@@ -1,11 +1,91 @@
 package com.leefo.budgetapplication.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 public class BudgetGrader {
 
-    TransactionModel transactionModel;
+    private final TransactionModel transactionModel;
 
     public BudgetGrader(TransactionModel transactionModel) {
         this.transactionModel = transactionModel;
+    }
+
+    public List<Category> getBudgetCategories() {
+        List<Category> budgetList = new ArrayList<>();
+        for (Category c : transactionModel.getCategoryList()) {
+            if (c.getGoal() > 0) {
+                budgetList.add(c);
+            }
+        }
+        return budgetList;
+    }
+
+    public float gradeCategory(Category category, int year, int month) {
+        float categoryBudget = category.getGoal();
+        float categoryActualExpenseSum = transactionModel.getTransactionSum(new TransactionRequest(category, month, year));
+
+        float budgetingOutcome = (categoryActualExpenseSum / categoryBudget);
+        return getGrade(budgetingOutcome);
+
+    }
+
+    private float getGrade(double budgetingOutcome) {
+        // Lower boundaries for grades
+
+        // Grade 5: 0-0.65
+        double grade5Lower = 0;
+
+        // Grade 4.5: 0.65-0.8
+        double grade4point5Lower = 0.65;
+
+        // Grade 4: 0.8-0.9
+        double grade4Lower = 0.8;
+
+        // Grade 3.5: 0.9-0.95
+        double grade3point5Lower = 0.9;
+
+        // Grade 3: 0.95-1.05
+        double grade3Lower = 0.95;
+
+        // Grade 2.5: 1.05-1.15
+        double grade2point5Lower = 1.05;
+
+        // Grade 2: 1.15-1.25.
+        double grade2Lower = 1.15;
+
+        // Grade 1.5: 1.25-1.45
+        double grade1point5Lower = 1.25;
+
+        // Grade 1: 1.45-1.60
+        double grade1Lower = 1.45;
+
+        // Grade 0.5: 1.60-2.0
+        double grade0point5Lower = 1.60;
+
+        // Grade 0: 2.0>
+        double grade0Lower = 2.0;
+
+        //Boundaries are lower inclusive => grade 4 includes grade4Lower to but not including
+        // grade3point5Lower
+        TreeMap<Double, Double> gradeMap = new TreeMap<>();
+        gradeMap.put(grade5Lower, 5.0);
+        gradeMap.put(grade4point5Lower, 4.5);
+        gradeMap.put(grade4Lower, 4.0);
+        gradeMap.put(grade3point5Lower, 3.5);
+        gradeMap.put(grade3Lower, 3.0);
+        gradeMap.put(grade2point5Lower, 2.5);
+        gradeMap.put(grade2Lower, 2.0);
+        gradeMap.put(grade1point5Lower, 1.5);
+        gradeMap.put(grade1Lower, 1.0);
+        gradeMap.put(grade0point5Lower, 0.5);
+        gradeMap.put(grade0Lower, 0.0);
+
+        double actualGrade = gradeMap.floorEntry(budgetingOutcome).getValue();
+        return (float) actualGrade;
+
+
     }
 
 
