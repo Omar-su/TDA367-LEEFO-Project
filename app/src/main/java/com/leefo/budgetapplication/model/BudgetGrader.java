d69e231ed412ca5a134273dad4a740909286d450
felixedholm <felix.edholm@gmail.com>
2021-10-12 09:25:19 +0200

    Created BudgetGrader class

---
+++ app/src/main/java/com/leefo/budgetapplication/model/BudgetGrader.java
@@ -0,0 +1,12 @@
+package com.leefo.budgetapplication.model;
+
+public class BudgetGrader {
+
+    TransactionModel transactionModel;
+
+    public BudgetGrader(TransactionModel transactionModel) {
+        this.transactionModel = transactionModel;
+    }
+
+
+}
