bafcb8fedbb70768d01489570a3fee6372ce66b4
felixedholm <felix.edholm@gmail.com>
2021-10-12 11:02:00 +0200

    Created BudgetGraderTest class

---
+++ app/src/test/java/com/leefo/budgetapplication/BudgetGraderTest.java
@@ -0,0 +1,29 @@
+package com.leefo.budgetapplication;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import android.provider.ContactsContract;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.leefo.budgetapplication.model.BudgetGrader;
+import com.leefo.budgetapplication.model.Category;
+import com.leefo.budgetapplication.model.DataBaseManager;
+import com.leefo.budgetapplication.model.IDatabase;
+import com.leefo.budgetapplication.model.TransactionModel;
+
+public class BudgetGraderTest {
+/*
+    @Test
+    public void canGetCorrectGrade1(){
+        IDatabase db = new DataBaseManager(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        TransactionModel tm = new TransactionModel(db);
+        BudgetGrader budgetGrader = new BudgetGrader(tm);
+
+        Category testCat = new Category()
+
+        float grade = budgetGrader.get
+    }*/
+}
