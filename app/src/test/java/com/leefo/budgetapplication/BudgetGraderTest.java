bad1f4af18845944401f73f09127c4389c5ec903
felixedholm <felix.edholm@gmail.com>
2021-10-12 11:33:46 +0200

    Changes to test

--- app/src/test/java/com/leefo/budgetapplication/BudgetGraderTest.java
+++ app/src/test/java/com/leefo/budgetapplication/BudgetGraderTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import static java.lang.Double.compare;
+
 import android.provider.ContactsContract;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -11,19 +13,27 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.leefo.budgetapplication.model.BudgetGrader;
 import com.leefo.budgetapplication.model.Category;
 import com.leefo.budgetapplication.model.DataBaseManager;
+import com.leefo.budgetapplication.model.FinancialTransaction;
 import com.leefo.budgetapplication.model.IDatabase;
 import com.leefo.budgetapplication.model.TransactionModel;
+import com.leefo.budgetapplication.model.TransactionRequest;
+
+import java.time.LocalDate;
 
 public class BudgetGraderTest {
+
 /*
     @Test
     public void canGetCorrectGrade1(){
-        IDatabase db = new DataBaseManager(InstrumentationRegistry.getInstrumentation().getTargetContext());
-        TransactionModel tm = new TransactionModel(db);
+        TransactionModel tm = new TransactionModel();
         BudgetGrader budgetGrader = new BudgetGrader(tm);
 
-        Category testCat = new Category()
+        Category testCat = new Category("Test", "#FFFFFF", false, 200);
+        FinancialTransaction testTransaction = new FinancialTransaction(150, "Test",
+                LocalDate.of(2021, 10, 12), testCat);
 
-        float grade = budgetGrader.get
+        //Outcome is 0.75 should be grade 4.5
+        float grade = budgetGrader.gradeCategory(new TransactionRequest(testCat, 10,2021));
+        assertEquals(compare(grade, 4.5), 0);
     }*/
 }
