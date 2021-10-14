06b5d9649d7495078845d5a7892e5305620f8e00
dubbell <46517201+dubbell@users.noreply.github.com>
2021-10-14 05:18:46 +0200

    implemented functionality for getting streak at specific date

--- app/src/main/java/com/leefo/budgetapplication/model/StreakCalculator.java
+++ app/src/main/java/com/leefo/budgetapplication/model/StreakCalculator.java
@@ -7,10 +7,90 @@ public class StreakCalculator
 {
 
 
+    /**
+     * Gets the streak of today.
+     * @param transactions List of transactions to base calculations off of.
+     * @return Spending streak of today.
+     */
+    public int getCurrentStreak(ArrayList<FinancialTransaction> transactions)
+    {
+        return getStreak(transactions, LocalDate.now());
+    }
+
+    public int getRecordStreak(ArrayList<FinancialTransaction> transactions)
+    {
+        int record = getCurrentStreak(transactions);
+
+
+
+        return record;
+    }
 
 
     /**
-     * Gets average expenses daily before specified date.
+     * Returns the streak of a specific date. A streak is a sequence of dates where the spending of each day is less than the previous average spending.
+     *
+     * @param transactions List of transactions to base calculations off of.
+     * @param date Date to check streak at.
+     * @return Returns total amount of days included in streak.
+     */
+    private int getStreak(ArrayList<FinancialTransaction> transactions, LocalDate date)
+    {
+        float average = getAverageDailySpending(transactions, date);
+
+        int streak = 0;
+        boolean streaking = true;
+
+        while(streaking)
+        {
+            float spending = getDaySpending(transactions, date);
+
+            if(spending < average)
+            {
+                streak++;
+                date = date.minusDays(1); // makes sure to check previous day in next iteration
+            }
+            else
+            {
+                // if a streak is ongoing, then the average spending should be going down after every day
+                // this means that when we reach a date where spending > average we have to see if  it also applies to the average spending
+                // before that date, since the average spending should be higher the further back in the streak we go
+
+                float newAverage = getAverageDailySpending(transactions, date);
+
+                if(newAverage == average) // if it comes back here again, we are no longer streaking
+                    streaking = false;
+                else
+                    average = newAverage;
+            }
+        }
+
+        return streak;
+    }
+
+    /**
+     * Gets total spending on a specific date.
+     *
+     * @param transactions List of transactions to check spending in.
+     * @param date Date to check spending on.
+     * @return Returns the total spending on specified date.
+     */
+    private float getDaySpending(ArrayList<FinancialTransaction> transactions, LocalDate date)
+    {
+        float sum = 0;
+
+        for(FinancialTransaction transaction : transactions)
+        {
+            if(transaction.getDate().isEqual(date))
+                sum += transaction.getAmount();
+        }
+
+        return sum;
+    }
+
+    /**
+     * Gets average daily expenses before specified date.
+     *
      * @param transactions List of transactions to calculate with.
      * @param date Date before which the transactions are made.
      * @return Average spending before date argument.
