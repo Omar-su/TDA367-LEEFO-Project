dd351809267cf7cf94fc4a006c6172a547d65a6d
dubbell <46517201+dubbell@users.noreply.github.com>
2021-10-15 08:55:40 +0200

    continued work on ui

--- app/src/main/java/com/leefo/budgetapplication/view/fragments/StreakFragment.java
+++ app/src/main/java/com/leefo/budgetapplication/view/fragments/StreakFragment.java
@@ -4,40 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.leefo.budgetapplication.R;
-import com.leefo.budgetapplication.controller.Controller;
-import com.leefo.budgetapplication.model.Category;
-import com.leefo.budgetapplication.view.ParcelableCategory;
 import com.leefo.budgetapplication.view.adapters.ManageCategoriesListAdapter;
 
-import java.util.ArrayList;
-
 /**
- * @author Emelie Edberg, Eugene Dvoryankov
+ * @author Linus Lundgren
  */
 public class StreakFragment extends Fragment {
 
-    private Button newCategoryButton;
-    private ListView listView;
-    private RadioGroup radioGroup;
-    private ManageCategoriesListAdapter adapter;
-    private ImageButton cross;
+    private TextView currentStreak, longestStreak;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_manage_categories, container, false);
 
-
+        currentStreak = view.findViewById(R.id.currentStreak);
+        longestStreak = view.findViewById(R.id.longestStreak);
 
         return view;
+    }
+
+    private void loadStreakValues()
+    {
+
     }
 
 
