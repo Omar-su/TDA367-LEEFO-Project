3f6828c0f4adec08c639540666a2622cd468ff92
dubbell <46517201+dubbell@users.noreply.github.com>
2021-10-15 09:55:36 +0200

    started on tests

---
+++ app/src/androidTest/java/com/leefo/budgetapplication/StreakTest.java
@@ -0,0 +1,18 @@
+package com.leefo.budgetapplication;
+
+import com.leefo.budgetapplication.model.FinancialTransaction;
+import com.leefo.budgetapplication.model.StreakCalculator;
+
+import java.util.ArrayList;
+
+
+public class StreakTest {
+
+    ArrayList<FinancialTransaction> transactions;
+
+    @Before
+    public void init()
+    {
+
+    }
+}
