146fbd0d899eabddaf6230cd4e34f7d5d16b2b52
Omar-su <73144494+Omar-su@users.noreply.github.com>
2021-10-16 23:01:15 +0200

    more splash screen implementation

--- app/src/main/java/com/leefo/budgetapplication/view/SplashScreenActivity.java
+++ app/src/main/java/com/leefo/budgetapplication/view/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import com.leefo.budgetapplication.R;
 
@@ -13,6 +14,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
