fcac07187f9aa5bebe7dc2526b440b3f462d5d47
Staol <kolontayev1@gmail.com>
2021-10-22 12:32:11 +0200

    Reformed the class. It now uses Singleton Pattern

--- app/src/main/java/com/leefo/budgetapplication/controller/CategoryController.java
+++ app/src/main/java/com/leefo/budgetapplication/controller/CategoryController.java
@@ -7,20 +7,35 @@ import java.util.ArrayList;
 
 /**
  * The CategoryController class represents a Controller in the Model-View-Controller pattern.
+ * The class uses Singleton design pattern and therefore has getInstance() method and private constructor
  * The class responsibility is to listen to the View and respond by modifying category data and
  * updating the view.
  *
- * @author Felix Edholm, Linus Lundgren, Emelie Edberg
+ * @author Felix Edholm, Linus Lundgren, Emelie Edberg, Eugene Dvoryankov
  */
 public class CategoryController {
+
+    /**
+     * The field for storing singleton instance
+     */
+    private static CategoryController INSTANCE = null;
 
     /**
      * Object handling logic for categories
      */
     private static CategoryModel categoryModel;
 
-    public CategoryController(CategoryModel categoryModel) {
-        CategoryController.categoryModel = categoryModel;
+    /**
+     * The singleton's constructor should always be private to avoid direct calls with 'new" operator
+     */
+    private CategoryController(){}
+
+    /** Returns single instance of the TransactionController class
+     *
+     * @return INSTANCE
+     */
+    public static CategoryController getInstance(){
+        return INSTANCE;
     }
 
     /**
@@ -35,6 +50,12 @@ public class CategoryController {
         categoryModel.editCategory(oldCategory, newCategory);
     }
 
+    /**
+     * Edits the name and color of a category with the given id. Color must be a String of a hexadecimal
+     * color code with the format: #XXXXXX.
+     *
+     * @param oldCategory Object of category to be changed.
+     */
     public static void editCategoryInfo(Category oldCategory, int goal) {
         Category newCategory = new Category(oldCategory.getName(), oldCategory.getColor(), oldCategory.isIncome(), goal);
 
@@ -53,7 +74,6 @@ public class CategoryController {
 
         categoryModel.addCategory(newCategory);
     }
-
 
     /**
      * Removes category with the given id from the database. Transactions under the removed category
